e6cd4e0e909416d27fb965890da796db02e4df5f
david <dmsim159@gmail.com>
2025-04-08 18:02:01 -0700

    install rest and app to installed apps

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'LittleLemonAPI',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
